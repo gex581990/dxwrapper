647b55b49927654739e5f914ac301ec98a38c789
Inasnum <129348281+gex581990@users.noreply.github.com>
2026-03-27 14:44:35 -0400

    debloat: remove Stub/, ddraw-testing/, Resources/, .github/

--- ddraw-testing/Logging.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "External\Logging\Logging.h"
-
-typedef enum _DDERR {} DDERR;
-
-std::ostream& operator<<(std::ostream& os, REFIID riid);
-std::ostream& operator<<(std::ostream& os, const DDERR& ErrCode);
-std::ostream& operator<<(std::ostream& os, const D3DCOLORVALUE& data);
